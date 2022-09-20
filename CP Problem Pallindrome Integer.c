bad364880c8adffe919edd891df143323dd01db2
SAHIL KHAN <110851333+Sahil97Khan@users.noreply.github.com>
2022-09-20 17:50:39 +0530

    Add files via upload

---
+++ CP Problem Pallindrome Integer.c
@@ -0,0 +1,35 @@
+///to check Pallindrome Integer...! Ex.121, 868
+
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int x;
+    printf("ENTER ANY THREE-DIGIT INTEGER:-");
+    scanf("%d",&x);
+
+    if(x<=0)
+    {
+        printf("\nFalse");
+        getch();
+    }
+
+    int y=x%10;
+
+    int z=x/100;
+
+
+    int a=x-z*100-y;
+
+    int b=y*100+a+z;
+
+  if(b==x)
+  {
+      printf("\nTrue");
+  }
+  else
+   {printf("\nFalse");}
+
+  getch();
+}
